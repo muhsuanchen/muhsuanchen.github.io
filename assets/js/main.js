711fc3f07a18ad7699654f72a9a0b14dca876747
muhsuanchen <wenny@wennychens-MacBook-Air.local>
2017-07-21 18:55:22 +0800

    lock body scrolling test.

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,14 +213,14 @@
 						}
 						modal.unbind('reveal:open');
 						//console.log('modal is open.');
-						if( navigator.userAgent.match(/Android/i)
+						/*navigator.userAgent.match(/Android/i)
 							 || navigator.userAgent.match(/webOS/i)
 							 || navigator.userAgent.match(/iPhone/i)
 							 || navigator.userAgent.match(/iPad/i)
 							 || navigator.userAgent.match(/iPod/i)
 							 || navigator.userAgent.match(/BlackBerry/i)
-							 || navigator.userAgent.match(/Windows Phone/i)
-							 ){
+							 || navigator.userAgent.match(/Windows Phone/i) */
+						if( window.innerWidth <= 960 ){
 						    $("body").addClass("modal-open-mobile");
 						} else {
 							console.log('desktop.');
@@ -267,14 +267,7 @@
 						modal.unbind('reveal:close');
 						//console.log('modal is close.');
 						
-						if( navigator.userAgent.match(/Android/i)
-							 || navigator.userAgent.match(/webOS/i)
-							 || navigator.userAgent.match(/iPhone/i)
-							 || navigator.userAgent.match(/iPad/i)
-							 || navigator.userAgent.match(/iPod/i)
-							 || navigator.userAgent.match(/BlackBerry/i)
-							 || navigator.userAgent.match(/Windows Phone/i)
-							 ){
+						if( window.innerWidth <= 960 ){
 						    $("body").removeClass("modal-open-mobile");
 						} else {
 							$("body").removeClass("modal-open");
